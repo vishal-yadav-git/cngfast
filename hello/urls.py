71c8d240b89fb24a585dd2db9bde6549676775e1
yadavvishal456456@gmail.com <yadavvishal456456@gmail.com>
2024-12-03 01:27:13 +0530

    file added

---
+++ hello/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for hello project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "CNG"
+admin.site.site_title = "CNG 2"
+admin.site.index_title = "Welcome to CNG Portal"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('signup/', include('home.urls')),
+    path('login/', include('home.urls')),
+]
+# add media file handling in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
